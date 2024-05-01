b30e8b58bd216e0c5a6e264a12cd443e8db6d496
Jarvy Lazan <j.lazan16@gmail.com>
2024-05-01 19:33:50 -0400

    Continued work on main form

--- GroupGenerator/Form1.Designer.cs
+++ GroupGenerator/Form1.Designer.cs
@@ -38,7 +38,14 @@
             importStudentsTextBox = new Button();
             deleteStudentButton = new Button();
             clearStudentsButton = new Button();
+            groupingGroupBox = new GroupBox();
+            membersInAGroupRadioButton = new RadioButton();
+            numberOfMembersRadioButton = new RadioButton();
+            groupSizeLabel = new Label();
+            groupSizeTextBox = new TextBox();
+            createGroupsButton = new Button();
             nameFormatSelectionGroupBox.SuspendLayout();
+            groupingGroupBox.SuspendLayout();
             SuspendLayout();
             // 
             // studentListBox
@@ -139,11 +146,73 @@
             clearStudentsButton.Text = "Clear";
             clearStudentsButton.UseVisualStyleBackColor = true;
             // 
+            // groupingGroupBox
+            // 
+            groupingGroupBox.Controls.Add(numberOfMembersRadioButton);
+            groupingGroupBox.Controls.Add(membersInAGroupRadioButton);
+            groupingGroupBox.Location = new Point(12, 775);
+            groupingGroupBox.Name = "groupingGroupBox";
+            groupingGroupBox.Size = new Size(426, 182);
+            groupingGroupBox.TabIndex = 7;
+            groupingGroupBox.TabStop = false;
+            groupingGroupBox.Text = "Choose how to group the students";
+            // 
+            // membersInAGroupRadioButton
+            // 
+            membersInAGroupRadioButton.AutoSize = true;
+            membersInAGroupRadioButton.Location = new Point(15, 54);
+            membersInAGroupRadioButton.Name = "membersInAGroupRadioButton";
+            membersInAGroupRadioButton.Size = new Size(386, 36);
+            membersInAGroupRadioButton.TabIndex = 0;
+            membersInAGroupRadioButton.TabStop = true;
+            membersInAGroupRadioButton.Text = "Number of members in a group";
+            membersInAGroupRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // numberOfMembersRadioButton
+            // 
+            numberOfMembersRadioButton.AutoSize = true;
+            numberOfMembersRadioButton.Location = new Point(15, 96);
+            numberOfMembersRadioButton.Name = "numberOfMembersRadioButton";
+            numberOfMembersRadioButton.Size = new Size(337, 36);
+            numberOfMembersRadioButton.TabIndex = 1;
+            numberOfMembersRadioButton.TabStop = true;
+            numberOfMembersRadioButton.Text = "Number of groups to make";
+            numberOfMembersRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // groupSizeLabel
+            // 
+            groupSizeLabel.AutoSize = true;
+            groupSizeLabel.Location = new Point(27, 989);
+            groupSizeLabel.Name = "groupSizeLabel";
+            groupSizeLabel.Size = new Size(62, 32);
+            groupSizeLabel.TabIndex = 8;
+            groupSizeLabel.Text = "Size:";
+            // 
+            // groupSizeTextBox
+            // 
+            groupSizeTextBox.Location = new Point(120, 982);
+            groupSizeTextBox.Name = "groupSizeTextBox";
+            groupSizeTextBox.Size = new Size(87, 39);
+            groupSizeTextBox.TabIndex = 9;
+            // 
+            // createGroupsButton
+            // 
+            createGroupsButton.Location = new Point(471, 931);
+            createGroupsButton.Name = "createGroupsButton";
+            createGroupsButton.Size = new Size(400, 90);
+            createGroupsButton.TabIndex = 10;
+            createGroupsButton.Text = "Create Group";
+            createGroupsButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(922, 979);
+            ClientSize = new Size(926, 1086);
+            Controls.Add(createGroupsButton);
+            Controls.Add(groupSizeTextBox);
+            Controls.Add(groupSizeLabel);
+            Controls.Add(groupingGroupBox);
             Controls.Add(clearStudentsButton);
             Controls.Add(deleteStudentButton);
             Controls.Add(importStudentsTextBox);
@@ -155,6 +224,8 @@
             Text = "Group Generator";
             nameFormatSelectionGroupBox.ResumeLayout(false);
             nameFormatSelectionGroupBox.PerformLayout();
+            groupingGroupBox.ResumeLayout(false);
+            groupingGroupBox.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -171,5 +242,11 @@
         private Button importStudentsTextBox;
         private Button deleteStudentButton;
         private Button clearStudentsButton;
+        private GroupBox groupingGroupBox;
+        private RadioButton numberOfMembersRadioButton;
+        private RadioButton membersInAGroupRadioButton;
+        private Label groupSizeLabel;
+        private TextBox groupSizeTextBox;
+        private Button createGroupsButton;
     }
 }
